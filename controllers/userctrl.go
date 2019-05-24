489987427f5a847679912fb775627018d85f5cb0
Asset Sultanov <asset.sultan@gmail.com>
2019-05-25 01:30:50 +0600

    Send Email, set JWT implemented

--- controllers/userctrl.go
+++ controllers/userctrl.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/jinzhu/gorm"
 	"github.com/kataras/iris"
+	"gopkg.in/dgrijalva/jwt-go.v3"
+	"gopkg.in/gomail.v2"
 )
 
 // User model
@@ -17,6 +19,13 @@ type User struct {
 	Email     string    `gorm:"unique_index"`
 	Password  string    `gorm:"type:varchar(100)"`
 }
+
+const mailHost = "smtp.gmail.com"
+const mailPort = 587
+const mailUser = "testyfy7@gmail.com"
+const mailPassword = "qwgqebnounjwnmfo"
+const jwtSecret = "ju$tTe$t1t"
+const sessionTime = 30
 
 // CreateUser method
 func CreateUser(ctx iris.Context) {
@@ -38,6 +47,22 @@ func CreateUser(ctx iris.Context) {
 	defer db.Close()
 
 	db.Create(&User{Firstname: user.Firstname, Lastname: user.Lastname, Email: user.Email, Password: user.Password})
+
+	// Configure the email message
+	htmlBody := fmt.Sprintf("<h3>Hello, %s %s, you are successfully registered on Personal Planner web app.</h3>", user.Firstname, user.Lastname)
+	m := gomail.NewMessage()
+	m.SetHeader("From", mailUser)
+	m.SetHeader("To", user.Email)
+	m.SetHeader("Subject", "Registration confirmation")
+	m.SetBody("text/html", htmlBody)
+
+	d := gomail.NewDialer(mailHost, mailPort, mailUser, mailPassword)
+
+	// Send the email
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
+
 	ctx.JSON(iris.Map{"msg": "New user successfully created"})
 }
 
@@ -88,7 +113,7 @@ func Login(ctx iris.Context) {
 
 	err := ctx.ReadJSON(&user)
 	if err != nil || (user.Email == "" && user.Password == "") {
-		ctx.StatusCode(400)
+		ctx.StatusCode(iris.StatusBadRequest)
 		ctx.JSON(iris.Map{"message": "Email and Password must be provided."})
 		return
 	}
@@ -105,9 +130,38 @@ func Login(ctx iris.Context) {
 	db.Where(&User{Email: user.Email, Password: user.Password}).First(&foundedUser)
 
 	if foundedUser.Email == user.Email && foundedUser.Password == user.Password {
-		ctx.JSON(iris.Map{"user": &foundedUser})
+		// Create the JWT key used to create the signature
+		var jwtKey = []byte(jwtSecret)
+
+		// Create a struct that will be encoded to a JWT.
+		// We add jwt.StandardClaims as an embedded type, to provide fields like expiry time
+		type Claims struct {
+			Firstname string `json:"firstname"`
+			jwt.StandardClaims
+		}
+
+		expirationTime := time.Now().Add(sessionTime * time.Minute)
+		// Create the JWT claims, which includes the firstname and expiry time
+		claims := &Claims{
+			Firstname: user.Firstname,
+			StandardClaims: jwt.StandardClaims{
+				// In JWT, the expiry time is expressed as unix milliseconds
+				ExpiresAt: expirationTime.Unix(),
+			},
+		}
+
+		// Declare the token with the algorithm used for signing, and the claims
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+		// Create the JWT string
+		tokenString, err := token.SignedString(jwtKey)
+		if err != nil {
+			ctx.StatusCode(iris.StatusInternalServerError)
+		}
+
+		ctx.JSON(iris.Map{"user": &foundedUser, "token": tokenString})
 	} else {
-		ctx.StatusCode(400)
+		ctx.StatusCode(iris.StatusBadRequest)
 		ctx.JSON(iris.Map{"msg": "Invalid Email or Password."})
 	}
 }
